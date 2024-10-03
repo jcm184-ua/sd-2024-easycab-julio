e1169b5fac250ff2ea6f1927feacbacf65db27f3
Julio Corbalán Moreno <jcm184@alu.ua.es>
2024-10-04 00:30:42 +0200

    Añadir información a EC_Customer.py y EC_S.py

---
+++ taxis/sensors/EC_S.py
@@ -0,0 +1,3 @@
+# haz un pequeño programa que con un hilo conecte con socket a un EC_DE, que tenga una bool global estado
+# y que en otro hilo cuando pulses una tecla (o con un menu) cambie de estado.  
+# El hilo del socket cada segundo comprobará la variable global y le envie el estado al taxi
