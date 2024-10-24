098427e6e509efb355680c720889c5bb108dc184
Julio Corbalán Moreno <jcm184@alu.ua.es>
2024-10-24 20:51:58 +0200

    Resiliencia EC_S.py

--- taxis/sensors/EC_S.py
+++ taxis/sensors/EC_S.py
@@ -33,37 +33,20 @@ def gestionarConexionTaxi():
 
     while True:
         try:
-            socket abrirSocketCliente(TAXI_ADDR)
+            socket = abrirSocketCliente(TAXI_ADDR)
+            while True:
+                # MEJORAR MENSAJE [EC_Sensor->EC_DE_?][OK]
+                mensaje = "OK" if estado else "KO"
+                enviarMensajeCliente(socket, mensaje)
+                time.sleep(1)
 
         except Exception as e:
             print(f"WARNING: SOCKET CAIDO: {e}.")
             time.sleep(3)
             print(f"INFO: Reintentando conexión...")
-    
-    try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((TAXI_IP, int(TAXI_PORT)))
-            print(f"Conectado a EC_DE en {TAXI_IP}:{TAXI_PORT}")
-            
-            # MEJORAR MENSAJE [EC_Sensor->EC_DE_?][OK]
-            while True:
-                mensaje = "OK" if estado else "KO"
-                message = mensaje.encode(FORMAT)
-                msg_length = len(message)
-                send_length = str(msg_length).encode(FORMAT)
-                send_length += b' ' * (HEADER - len(send_length))
-                s.send(send_length)
-                s.send(message)
-                #s.sendall(mensaje.encode('utf-8'))
-                time.sleep(1)
-    except Exception as e:
-        print(f"SOCKET CAIDO {e}")
 
+#TODO: PONER EL DE PEDRE
 def cambiar_estado():
-    # haz un pequeño programa que con un hilo conecte con socket a un EC_DE, que tenga una bool global estado
-    # y que en otro hilo cuando pulses una tecla (o con un menu) cambie de estado.  
-    # El hilo del socket cada segundo comprobará la variable global y le envie el estado al taxi
-    """Función para cambiar el estado del sensor"""
     global estado
     while True:
         opcion = input("\nMenú:\n1. Cambiar estado\n2. Salir\nSelecciona una opción: ")
@@ -85,7 +68,6 @@ def main():
     hilo_socket = threading.Thread(target=gestionarConexionTaxi)
     hilo_socket.start()
 
-    #TODO: Hacer bien y en ventana extra
     cambiar_estado()
 
 if __name__ == "__main__":
