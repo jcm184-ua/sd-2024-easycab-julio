971dbe77a8a530400d22cb46c7662d9f2dd98136
Julio Corbalán Moreno <jcm184@alu.ua.es>
2024-10-25 19:28:45 +0200

    Completar BBDD

--- core/central/resources/iniciarBBDD.sql
+++ core/central/resources/iniciarBBDD.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS taxis (
     id TEXT PRIMARY KEY,
     estado TEXT NOT NULL default "desconectado" CHECK(estado = "desconectado" or estado = "esperando" or estado = "servicio"),
     sensores TEXT NOT NULL default "KO" CHECK(sensores = "OK" or sensores = "KO"),
-    posicion TEXT NOT NULL default ["-,-"],
+    posicion TEXT NOT NULL,
     cliente TEXT default NULL CHECK(cliente IS NULL or cliente = "a" or cliente = "b" or cliente = "c" or cliente = "d" or cliente = "e" or cliente = "f"),
     destino TEXT default NULL CHECK(destino IS NULL or destino = "A" or destino = "B" or destino = "C" or destino = "D" or destino = "E" or destino = "F")
 );
@@ -15,19 +15,16 @@ CREATE TABLE IF NOT EXISTS clientes (
     posicion TEXT NOT NULL default ["-,-"]
 );
 
-INSERT INTO taxis (id) VALUES (1);
-INSERT INTO taxis (id) VALUES (2);
-INSERT INTO taxis (id) VALUES (3);
-INSERT INTO taxis (id) VALUES (5);
-INSERT INTO taxis (id) VALUES (6);
-INSERT INTO taxis (id) VALUES (7);
-INSERT INTO taxis (id) VALUES (8);
-INSERT INTO taxis (id) VALUES (9);
-INSERT INTO taxis (id) VALUES (10);
+INSERT INTO taxis (id, posicion) VALUES (1, "1,1");
+INSERT INTO taxis (id, posicion) VALUES (2, "1,2");
+INSERT INTO taxis (id, posicion) VALUES (3, "1,3");
+INSERT INTO taxis (id, posicion) VALUES (4, "1,4");
+INSERT INTO taxis (id, posicion) VALUES (5, "1,5");
+INSERT INTO taxis (id, posicion) VALUES (6, "1,6");
 
-INSERT INTO clientes (id) VALUES ("a");
-INSERT INTO clientes (id) VALUES ("b");
-INSERT INTO clientes (id) VALUES ("c");
-INSERT INTO clientes (id) VALUES ("d");
-INSERT INTO clientes (id) VALUES ("e");
-INSERT INTO clientes (id) VALUES ("f");
+INSERT INTO clientes (id, posicion) VALUES ("a", "15,17");
+INSERT INTO clientes (id, posicion) VALUES ("b", "6,12");
+INSERT INTO clientes (id, posicion) VALUES ("c", "18,15");
+INSERT INTO clientes (id, posicion) VALUES ("d", "3,5");
+INSERT INTO clientes (id, posicion) VALUES ("e", "7,8");
+INSERT INTO clientes (id, posicion) VALUES ("f", " 13, 14");
