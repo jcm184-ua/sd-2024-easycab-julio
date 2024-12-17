d718a3c47e876000c47302fe25869bccd801d479
Álvaro Pedreño Rubio <alvaropedrenorubio@gmail.com>
2024-12-17 12:59:08 +0100

    Registry  parametrizado

--- core/central/EC_Registry.py
+++ core/central/EC_Registry.py
@@ -11,15 +11,19 @@ from EC_Shared import *
 DATABASE_USER = 'ec_registry'
 DATABASE_PASSWORD = 'sd2024_registry'
 
+LISTEN_PORT = None
+
 app = Flask(__name__)
 
-#NO FUNCIONA BIEN, NO TIENE QUE DEVOLVER TOKEN, SOLO AÑADIRLO A LA BBDD
-""" PUT http://127.0.0.1:5001/registrar ->
-<!doctype html>
-<html lang=en>
-<title>415 Unsupported Media Type</title>
-<h1>Unsupported Media Type</h1>
-<p>Did not attempt to load JSON data because the request Content-Type was not &#39;application/json&#39;.</p> """
+def comprobarArgumentos(argumentos):
+    if len(argumentos) != 2:
+        exitFatal("Necesito estos argumentos: <LISTEN_PORT>")        
+    printInfo("Número de argumentos correcto.")
+
+def asignarConstantes(argumentos):
+    global LISTEN_PORT
+    LISTEN_PORT = int(argumentos[1])
+    printInfo("Constantes asignadas.")
 
 @app.route("/registrar/<taxi_id>", methods=["PUT"])
 def registrarTaxi(taxi_id):
@@ -87,29 +91,9 @@ def verificarEstadoRegistro(taxi_id):
         return jsonify({"id": taxi["id"], "token": taxi["token"]}), 200
     return jsonify({"error": f"Taxi {taxi_id} no está registrado"}), 404
 
-# NO HAY QUE VALIDAR NADA. Los tokens son entre central y taxi
-"""
-@app.route("/validar/<taxi_id>", methods=["POST"])
-def validarToken(taxi_id):
-    #Valida el token enviado por un taxi a la central.
-    
-    data = request.get_json()
-    token = data.get("token")
-
-    if not token:
-        return jsonify({"error": "Token es requerido"}), 400
-
-    conexion, cursor = generarConexionBBDD(DATABASE_USER, DATABASE_PASSWORD)
-
-    cursor.execute("SELECT * FROM taxis WHERE id = %s AND token = %s", (taxi_id, token))
-    taxi = cursor.fetchone()
-
-    conexion.close()
-    if taxi:
-        return jsonify({"message": "Token válido"}), 200
-    return jsonify({"error": "Token inválido o taxi no registrado"}), 401
-"""
-
 if __name__ == "__main__":
+    comprobarArgumentos(sys.argv)
+    asignarConstantes(sys.argv)
+
     printInfo("Iniciando EC_Registry...")
-    app.run(debug=True, host="0.0.0.0", port=5001)
+    app.run(debug=True, port=LISTEN_PORT)
